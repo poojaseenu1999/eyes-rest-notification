3a85a875367936d0cee5db28102a573ef72aff44
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-31 00:36:24 +0530

    first commit

---
+++ eyes.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+
+notification.notify(
+title="REST YOUR EYES",
+message="Warning!give an rest to your eyes , its straining too hard!!",
+app_icon="C:\\Users\\Intel.DESKTOP-Q9LCO1R\\Desktop\\python\\eyesrest\\eye.ico",
+ timeout=20
+)
+time.sleep(8)
